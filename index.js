e4124c3840f848b7f3559a385c586398cb35669c
seemasheikh <98578905+seemasheikh@users.noreply.github.com>
2022-06-14 00:18:07 -0700

    my assignment 1 to 5

---
+++ index.js
@@ -0,0 +1,317 @@
+// chapter=1
+// Q-no-1;
+// alert("Wishing you a very happy eid mubarak");
+
+// q-no-2;
+// alert("Error! please enter avalid password.");
+
+// q-no-3;
+// alert("Welcome to js Land ...                                                                                                                                      "+
+// "happy coding !")
+
+
+// q-no-4(a);
+
+// alert("Welcome to js Land");
+
+// q-no-4(b);
+
+// alert("Happy coding                                                                                "+
+// "prevent this page creating from  additional dialouges");
+// q-no-5;
+
+// console.log("Hello... i can run through my web browser's console");
+
+// q-no-6;
+// see html page
+
+// q-no-7;
+// script tag in html
+// see index.html
+// -----------end of chapter 1---------
+
+
+// -----CHAPTER-2-----
+
+
+// q-no-1;
+// var userName="sara";
+// console.log(seUrName);
+
+// q-no-2;
+// var myName="My name is seema sheikh";
+// alert(myName);
+
+// q-no-3;
+// var message="Hello world";
+// alert(message);
+
+// q-no-4(a)
+// var studentName="sara sheikh";
+// alert(studentName);
+// q-no-4(b)
+// var studentAge="15 year old";
+// alert(studentAge);
+// var studentCourse="certified mobile application developement";
+// alert(studentCourse);
+
+// q-no-5
+
+// var str="pizza";
+// for(var i=str.length;i>=0;i--){
+//       console.log(str.slice(0,i));
+// }
+
+
+
+
+// q-no-6
+// var email="My email address is "+"example@example.com";
+// alert(email);
+
+// q-no-7
+// var book="a smarter way to learn javscript";
+// alert("I am trying to learn"+book)
+
+// q-no-8
+// document.write("yah!i can write html content through javascript")
+
+// q-n0-9
+// alert("--------------===$$&$$===-----------")
+
+
+// ---------END OF CHAPTER 2---------
+
+// CHAPTER=3--------
+// q-no-1
+// var age="15 years";
+// alert("I am "+age+" old");
+
+// q-no-2
+// var times="14";
+// alert("You have visited "+times+" times");
+
+// q-no-3
+// var birthYear=1990;
+// console.log("My birthYear is "+birthYear)
+
+// q-no-4
+
+// var visitorsName="johnDoe";
+// var quantity="5"
+// var productTitle="t-shirt";
+// var store="on xyz clothing store";
+// console.log(visitorsName + quantity +productTitle +store)
+
+// -----end of chapter #3---
+
+// CHAPTER=4---------
+
+// q-no-1
+// var weight=prompt("what's your weight?");
+// var time=prompt("what time you are available to workout");
+// var age=prompt("what is yourage");
+// if(weight>100&&time>6 &&age>17){
+//     alert("Come and try workout!")
+// }else{
+//     alert("Do not come")
+// }
+// q-no-2
+
+// 5-legal- variableNames
+// var myvar=10;
+// var abcBreak=10;
+// var userAlert=10;
+// var topsConsole=10;
+// var name="sara";
+
+// 5-illegal-variable names
+
+// var variable=1;
+// var my name="sara";
+// var alert="abc";
+// var break=123;
+// var this time=12 noon
+
+// q-no=3
+//  CHAPTER=4
+//  q-no-3 
+// document.write("<h1>Rules for naming js variable</h1>")
+// document.write("variable name can only contain,numbers,$ and_.for example:$my_1stvariable")
+// document.write("variable must begin with a letter,$ or_ for example:$name,_name or name")
+// document.write("variable names are case snsitive")
+// document.write("variable name should not be js key words")
+
+
+// --------end of chapter 4------
+
+// chapter=5---------
+// q-no-1
+// var num=3;
+// var newNum=5;
+// var add=num+newNum;
+// alert("Sum of 3 and 5 is "+add)
+
+// q-no-2(a)
+// var sum=20;
+// var sum1=4;
+// var answer=sum-sum1;
+// alert("Sum of 20 - 4 is "+answer)
+
+
+// q-no-2(b)
+// var orignalNum=3;
+// var orignal2=5;
+// var myOrignal=orignalNum*orignal2;
+// alert("Sum of 3 * 5 is "+myOrignal)
+
+// q-no-2(c)
+// var money=3;
+// var myMoney=5;
+// var oriMoney=money/myMoney;
+// alert("Sum of 3 /5 is "+oriMoney);
+
+// q-no-2(d)
+// var abs=3;
+// var def=5;
+// var plus=abs%def;
+// alert("Sum of 3 % 5 is "+plus)
+
+// q-no-3(a)
+
+// var val=0;
+// console.log("Value after variable declearation is"+val)
+
+// q-no-3(b)
+
+// var rupees=5;
+// console.log("Initial value:"+rupees)
+
+// q-no-3(c)
+// var increment=6;
+// console.log("Value of after increment is"+increment)
+
+// q-no-3(d)
+// var addition=13;
+// console.log("Value of after addition is "+addition)
+
+// q-no-3(e)
+// var decrement=12;
+// console.log("Value after decrement is "+decrement);
+
+// q-no-3(f)
+// var aa=0;
+// var bb=3;
+// var rim=aa/bb;
+// console.log("The remainder is"+rim)
+
+// q-no-4
+
+// var ticketPrice=600+"PKR";
+// var buyingTickets=5;
+// var cost=ticketPrice*buyingTickets;
+// console.log("Total cost to buy a movie is "+cost)
+
+//  q-no-5
+// table of 4
+
+// var table1=4;
+// console.log("4*1="+table1)
+// var table2=8;
+// console.log("4*2="+table2)
+// var table3=12;
+// console.log("4*3="+table3)
+// var table4=16;
+// console.log("4*4="+table4)
+// var table5=20;
+// console.log("4*5="+table5)
+// var table6=24;
+// console.log("4*6="+table6)
+// var table7=28;
+// console.log("4*7="+table7)
+// var table8=32;
+// console.log("4*8="+table8)
+// var table9=36;
+// console.log("4*9="+table9)
+// var table10=40;
+// console.log("4*10="+table10)
+
+// q-n0-6
+
+// var f=(c*9/5)+32;
+// alert(5/9*(68-32))
+
+// var c=(f-32)*5/9;
+// alert(36/1.8*(5/9)+68)
+
+// q-no-7
+
+// var a=650;
+// var b=3;
+// var c=100;
+// var d=7;
+// var f=100;
+// var totalCost=a*b+c*d+f;
+// console.log(totalCost)
+// console.log("Price of item1 is "+a)
+// console.log("Quantity  of item1 is "+b)
+// console.log("Price of item2 is "+c)
+// console.log("Quantity of item2 is "+d)
+// console.log("Shipping charges "+f)
+// console.log("Total cost of your order is "+totalCost)
+
+// q-no-8
+// var totalMarks=980;
+// var marksObtained=804;
+// var percentage=marksObtained*100/totalMarks;
+// console.log(percentage)
+
+
+// q-no-9
+// var usDollar=10;
+// var exchangeRate=104.80;
+// var  usdInPkr=usDollar*exchangeRate;
+// var saudRial=25;
+// var exchRate=28;
+// var saudInPkr=saudRial*exchRate;
+// console.log(SaudInPkr+usdInPkr)
+
+// q-no-10
+// var add=5;
+// var multiply=10;
+// var divide=2;
+// var x=add*multiply/divide;
+// console.log(x)
+
+// q-no-11
+// var currentYear=2016;
+// var birthYear=1992;
+// var yourAge=currentYear-birthYear;
+// console.log(YourAge)
+
+// q-no-12
+// var radius=20;
+// var cirm=125.67999999999999
+// var theAreaIs=radius*cirm/2;
+// console.log(theAreaIs)
+
+// q-no-13
+// var favouriteSnack="chocolateChip";
+// var currentAge=15;
+//  var estimatedMaximumAGE=65;
+//  var amountOfSnackPerDay=3;
+//  var x=estimatedMaximumAGE*amountOfSnackPerDay-currentAge*amountOfSnackPerDay;
+// console.log("You will need "+x+" chocolatechip to last until the ripe age of "+estimatedMaximumAGE)
+
+
+
+
+
+
+
+
+
+
+
+
+
